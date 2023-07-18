3dacd1629ffb3ac86ed88efa6fd17f03a79564ae
Ferran Maylinch <ferran@yandex-team.ru>
2023-07-18 11:47:52 +0300

    Remove also gray color from terraform output

--- terraform_util.sh
+++ terraform_util.sh
@@ -5,6 +5,7 @@
 # https://gist.github.com/JBlond/2fea43a3049b38287e5e9cefc87b2124
 reset=$(printf '\33\\[0m')
 black=$(printf '\33\\[30m')
+grey=$(printf '\33\\[90m')
 red=$(printf '\33\\[31m')
 green=$(printf '\33\\[32m')
 yellow=$(printf '\33\\[33m')
@@ -25,7 +26,8 @@ tfcolor() {
     else
         # remove color reset, put color reset at the end
         # replace fg color with bg color
-        # remove remaining fg colors so they don't break bg color (necessary?)
+        # remove remaining fg colors (to see $black over bg colors)
+        # TODO: join regex for fg colors (to remove them all except $black)
         sed -u "s/$reset//g" \
         | sed -u "s/$/$reset/" \
         | sed -u "s/$green/$greenbg$black/" \
@@ -33,7 +35,8 @@ tfcolor() {
         | sed -u "s/$yellow/$yellowbg$black/" \
         | sed -u "s/$green//g" \
         | sed -u "s/$red//g" \
-        | sed -u "s/$yellow//g"
+        | sed -u "s/$yellow//g" \
+        | sed -u "s/$grey//g"
     fi
 }
 
